45df0316816136aa4a8d83f31aeb45e048d27ce7
Michel Figueira <michelfigueiratemp@gmail.com>
2021-08-21 10:48:20 -0300

    componentRef in domElement

--- src/app/shared/services/modal.service.ts
+++ src/app/shared/services/modal.service.ts
@@ -1,23 +1,37 @@
-import { Injectable } from '@angular/core';
+import { ComponentFactory, ComponentFactoryResolver, ComponentRef, Injectable, Injector } from '@angular/core';
+import { ModalComponent } from '../components/modal/modal.component';
 import { ModalConfig } from '../interfaces/modal-config';
+import { BodyInjectorService } from './body-injector.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
 
-  constructor() { }
+  private componentFactory: ComponentFactory<ModalComponent>;
+
+  constructor(componentFactoryResolver: ComponentFactoryResolver, private injector: Injector, private bodyInjector: BodyInjectorService) {
+    this.componentFactory = componentFactoryResolver.resolveComponentFactory(ModalComponent);
+  }
 
   public open(config: ModalConfig): ModalRef {
-    console.log('open called');
-    return new ModalRef();
+    const componentRef = this.createComponentRef();
+    componentRef.instance.config = config;
+    this.bodyInjector.stackBeforeAppRoot(componentRef);
+    return new ModalRef(componentRef);
+  }
+
+  public createComponentRef(): ComponentRef<ModalComponent> {
+    return this.componentFactory.create(this.injector);
   }
 }
 
 export class ModalRef {
 
+  constructor(private componentRef: ComponentRef<ModalComponent>) {}
+
   public close(): void {
-    console.log('close called');
+    this.componentRef.destroy();
   }
 
 }
